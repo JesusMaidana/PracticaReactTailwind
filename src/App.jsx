b040f1148a003c86588930eeacbf99db1f3491c0
jesus <jesusmaidana35@gmail.com>
2024-07-25 17:16:24 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./componentes/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/images/logo.svg'
+
+export const Header = () => {
+  return (
+    <header>
+        <img src= {logo} alt="Logo" />
+    </header>
+  )
+}
