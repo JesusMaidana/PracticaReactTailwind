40576fc178e819df6dc21d081f96e4f97ed89bd7
jesus <jesusmaidana35@gmail.com>
2024-07-25 17:24:15 -0300

    creacion del navbar

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,12 @@
 import logo from '../assets/images/logo.svg'
+import { Navbar } from './Navbar'
 
 export const Header = () => {
   return (
     <header>
         <img src= {logo} alt="Logo" />
+        <Navbar />
     </header>
+
   )
 }
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,21 @@
+
+
+
+export const Navbar = () => {
+  return (
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Popular</a>
+        </li>
+        <li>
+            <a href="#">Traending</a>
+        </li>
+        <li>
+            <a href="#">Categories</a>
+        </li>
+    </ul>
+  )
+}
