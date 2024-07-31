086e8a029550098cc51584ea08b09f16bd529791
Jesús <jesusmaidana35@gmail.com>
2024-07-31 11:41:46 -0300

    Se genero una nueva seccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Header } from "./componentes/Header";
 import { MainArticle } from "./componentes/MainArticle";
 import { NewContainer } from "./componentes/NewContainer";
+import { ArticlesContainer } from "./componentes/ArticlesContainer";
 
 function App() {
   return (
     <main className="px-4 pt-6">
       <Header />
-      <div className="sm:flex sm:gap-8">
+      <div className="lg:flex lg:gap-8">
         <MainArticle />
         <NewContainer/>
       </div>
+      <ArticlesContainer/>
+        
     </main>
   );
 }
