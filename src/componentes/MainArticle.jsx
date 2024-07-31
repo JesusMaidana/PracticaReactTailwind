1f014142813aa5926725af69ff3311a0f070fb7b
jesus <jesusmaidana35@gmail.com>
2024-07-31 13:29:38 -0300

    fin del reto 2

--- src/componentes/MainArticle.jsx
+++ src/componentes/MainArticle.jsx
@@ -12,7 +12,7 @@ export const MainArticle = () => {
 
       <div className='sm:flex'>
         <div className="flex-1 py-6">
-          <h2 className='text-[40px] font-bold sm:text-[58px] leading-none'>The Bright Future of Web 3.0?</h2>
+          <h2 className='text-[41px] font-bold sm:text-[58px] leading-none'>The Bright Future of Web 3.0?</h2>
         </div>
         <div className='flex-1 pt-9'>
           <p className='text-[13px] mb-10 sm:text-[15px]'>
