edef3213dc7fdb1412d80a79696fb6251f3ab81f
jesus <jesusmaidana35@gmail.com>
2024-08-05 16:36:54 -0300

    Se genero una nueva rama jesus2 y se separo el modulo MainArticleText del modulo MainArticle para trabajarlo por sepado, esto facilita la integracion con swiper para la animacion del slider. Se intalo a traves de npm swiper y react-icons

--- src/componentes/MainArticle.jsx
+++ src/componentes/MainArticle.jsx
@@ -1,5 +1,14 @@
-import imageMobile from "../assets/images/image-web-3-mobile.jpg";
-import imageDesktop from "../assets/images/image-web-3-desktop.jpg";
+import imageMobile from "../assets/images/1.webp";
+import imageDesktop from "../assets/images/1.webp";
+import { MainArticleText } from "./MainArticleText";
+
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/free-mode'
+
+import {FreeMode, Pagination} from 'swiper/modules'
+import {RxArrowTopRight} from 'react-icons/rx'
 
 export const MainArticle = () => {
   return (
@@ -10,19 +19,7 @@ export const MainArticle = () => {
         <img src={imageMobile} alt="Articulo principal imagen" />
       </picture>
 
-      <div className='sm:flex'>
-        <div className="flex-1 py-6">
-          <h2 className='text-[41px] font-bold sm:text-[58px] leading-none'>The Bright Future of Web 3.0?</h2>
-        </div>
-        <div className='flex-1 pt-9'>
-          <p className='text-[13px] mb-10 sm:text-[15px]'>
-            We dive into the next evolution of the web that claims to put the
-            power of the platforms back into the hands of the people. But is it
-            really fulfilling its promise?
-          </p>
-          <button className="bg-SoftRed w-[185px] h-[48px] uppercase text-OffWhite hover:bg-VeryDarkBlue">Read more</button>
-        </div>
-      </div>
+      <MainArticleText/>
     </section>
   );
 };
