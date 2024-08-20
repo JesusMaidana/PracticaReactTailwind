693c4c2640132dfaecaad040f5e3e25d1dee281b
Jesús <jesusmaidana35@gmail.com>
2024-08-20 20:59:39 -0300

    Se agrego el menu hamburguesa y un icono se cambio el color de los hovers al pasar el mouse

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import burgerMenu from "../assets/images/icon-menu.svg";
+import closeMenuIcon from "../assets/images/icon-menu-close.svg"; // Opcional: icono de cierre
 
 export const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <ul className="hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center">
@@ -19,7 +27,32 @@ export const NavBar = () => {
         </li>
       </ul>
 
-      <img className="w-10 h-4 cursor-pointer sm:hidden" src={burgerMenu} alt="Menu hamburguesa"/>
+      <img
+        className="w-10 h-4 cursor-pointer sm:hidden"
+        src={menuOpen ? closeMenuIcon : burgerMenu}
+        alt="Menu hamburguesa"
+        onClick={toggleMenu}
+      />
+
+      {menuOpen && (
+        <ul
+          className="flex flex-col text-[18px] sm:hidden bg-DarkGrayishBlue text-white p-4 absolute top-16 right-0 z-50 shadow-lg"
+          // Aplicamos z-index alto para que esté por encima de otros elementos
+        >
+          <li className="mb-4">
+            <Link to="/video" onClick={toggleMenu}className="hover:text-black">Videos</Link>
+          </li>
+          <li className="mb-4">
+            <Link to="/fotos" onClick={toggleMenu}className="hover:text-black">Fotos</Link>
+          </li>
+          <li className="mb-4">
+            <Link to="/show" onClick={toggleMenu}className="hover:text-black">Shows</Link>
+          </li>
+          <li className="mb-4">
+            <Link to="/contacto" onClick={toggleMenu}className="hover:text-black">Contacto</Link>
+          </li>
+        </ul>
+      )}
     </>
   );
 };
