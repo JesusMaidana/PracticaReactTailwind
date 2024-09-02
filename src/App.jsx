324760a8019bfcc2a5a6747e9f3d9489ff1021ee
Jesús <jesusmaidana35@gmail.com>
2024-09-02 00:36:22 -0300

    se posiciono el footer siempre abajo en todas las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import { Header } from "./componentes/Header";
-import { Footer } from "./componentes/Footer"; // Asegúrate de tener este archivo creado
+import { Footer } from "./componentes/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Inicio from './pages/Inicio';  
-import Video from './pages/Video';
-import Fotos from './pages/Fotos';
-import Show from './pages/Show';
-import Contacto from './pages/Contacto';
+import Inicio from "./pages/Inicio";
+import Video from "./pages/Video";
+import Fotos from "./pages/Fotos";
+import Show from "./pages/Show";
+import Contacto from "./pages/Contacto";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main className="px-4 pt-6">
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/fotos" element={<Fotos />} />
-          <Route path="/show" element={<Show />} />
-          <Route path="/contacto" element={<Contacto />} />
-        </Routes>
-      </main>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow px-4 pt-6">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/video" element={<Video />} />
+            <Route path="/fotos" element={<Fotos />} />
+            <Route path="/show" element={<Show />} />
+            <Route path="/contacto" element={<Contacto />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
