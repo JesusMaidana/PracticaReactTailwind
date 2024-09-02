601f2c6c82fd5c6c5b431ef7a3c6a8ee84467f39
Jesús <jesusmaidana35@gmail.com>
2024-09-02 00:14:01 -0300

    se agrego el logo de shukran

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,12 +1,16 @@
-import { Link } from 'react-router-dom';
-import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logoshukran.svg";
 import { NavBar } from "./NavBar";
 
 export const Header = () => {
   return (
     <header className="flex place-content-between items-center mb-2 mt-4 mx-2.5">
       <Link to="/">
-        <img src={logo} alt="Logo" />
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-24 h-auto" // Ajusta el tamaño aquí
+        />
       </Link>
       <NavBar />
     </header>
