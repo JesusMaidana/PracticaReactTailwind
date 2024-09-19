9713e4489476e861c32956e26b5a36837599c095
Jesús <jesusmaidana35@gmail.com>
2024-09-19 20:36:46 -0300

    Se cambio el texto del formulario para el festival

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 export const Formulario = () => {
   return (
     <div className="bg-SoftRed text-OffWhite text-center py-4">
-      <h2 className="text-2xl font-bold">Inscripción para Fotos y Videos del Festival SOUTH AMERICA BELLYDANCE</h2>
+      <h2 className="text-2xl font-bold">Valor exclusivo SOUTH AMERICA BELLYDANCE</h2>
       <p className="mt-2 mb-4">
-        Preinscribite para asegurar tu compra de fotos y videos profesionales del festival. Recordá que, además de preinscribirte, deberás abonar y enviar el comprobante de pago.
+      • Reservá tu registro profesional de fotos y videos del festival. <br />
+      • Si sos solista o coordinadora de un grupo y querés asegurar tu compra de fotos y videos, hacé clic en el siguiente enlace y completa el formulario.
+      <br />• Recordá que, además de preinscribirte, deberás abonar y enviar el comprobante de pago.
       </p>
       <a 
         href="https://docs.google.com/forms/d/e/1FAIpQLSeddSSmxAFEm4B1wSAi_szBh49ePxJYfljmbexnBbHcHh_mdQ/viewform" 
