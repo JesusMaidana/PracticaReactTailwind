776714855df1c29c4b95570fd0fea3416d1c7c7f
Jesús <jesusmaidana35@gmail.com>
2024-09-19 19:49:57 -0300

    Se corrigio el footer se agrego el wsp de shukran y el instagram

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
 export const Footer = () => {
-  // src/socialLinks.js
-  const socialLinks = {
-    facebook: "https://www.facebook.com/yourpage",
-    twitter: "https://www.twitter.com/yourprofile",
-    instagram: "https://www.instagram.com/shukran.audiovisual/",
-    // Añade más enlaces aquí si es necesario
-  };
-
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between gap-8">
@@ -16,14 +8,8 @@ export const Footer = () => {
         <div className="flex flex-col gap-4">
           <h3 className="text-xl font-semibold">Síguenos</h3>
           <div className="flex gap-4">
-            <a href="#" className="text-gray-400 hover:text-white">
-              Facebook
-            </a>
-            <a href="#" className="text-gray-400 hover:text-white">
-              Twitter
-            </a>
             <a
-              href={socialLinks.instagram}
+              href="https://www.instagram.com/shukran.audiovisual/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-400 hover:text-white"
@@ -33,39 +19,18 @@ export const Footer = () => {
           </div>
         </div>
 
-        {/* Enlaces */}
-        <div className="flex flex-col gap-4">
-          <h3 className="text-xl font-semibold">Explora</h3>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="text-gray-400 hover:text-white">
-                UI Design
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-400 hover:text-white">
-                UX Design
-              </a>
-            </li>
-            {/* Añade más enlaces aquí */}
-          </ul>
-        </div>
-
         {/* Recursos */}
         <div className="flex flex-col gap-4">
-          <h3 className="text-xl font-semibold">Recursos</h3>
+          <h3 className="text-xl font-semibold">Contacto</h3>
           <ul className="space-y-2">
             <li>
-              <a href="#" className="text-gray-400 hover:text-white">
-                Blog
+              <a
+                href="https://wa.me/5491136667335"
+                className="text-gray-400 hover:text-white"
+              >
+                Habla con nosotros
               </a>
             </li>
-            <li>
-              <a href="#" className="text-gray-400 hover:text-white">
-                Support
-              </a>
-            </li>
-            {/* Añade más recursos aquí */}
           </ul>
         </div>
       </div>
