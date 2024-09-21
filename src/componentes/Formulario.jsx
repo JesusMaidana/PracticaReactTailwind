a440cb703b98a6dca8afddb86256ae36b4409665
Jesús <jesusmaidana35@gmail.com>
2024-09-21 12:27:25 -0300

    Se agregaron los videos a la pagina de producciones

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export const Formulario = () => {
   return (
-    <div className="bg-SoftRed text-OffWhite text-center py-4">
-      <h2 className="text-2xl font-bold">Valor exclusivo SOUTH AMERICA BELLYDANCE</h2>
+    <div className="bg-SoftRed text-OffWhite text-center py-4 rounded-2xl">
+      <h2 className="text-2xl font-bold">SOUTH AMERICA BELLYDANCE  </h2>
+      <h3 className='text-2xl'>Valor exclusivo</h3>
       <p className="mt-2 mb-4">
+        
       • Reservá tu registro profesional de fotos y videos del festival. <br />
       • Si sos solista o coordinadora de un grupo y querés asegurar tu compra de fotos y videos, hacé clic en el siguiente enlace y completa el formulario.
-      <br />• Recordá que, además de preinscribirte, deberás abonar y enviar el comprobante de pago.
+     
       </p>
+      {/* Recordá que, además de preinscribirte, deberás abonar y enviar el comprobante de pago. */}
       <a 
         href="https://docs.google.com/forms/d/e/1FAIpQLSeddSSmxAFEm4B1wSAi_szBh49ePxJYfljmbexnBbHcHh_mdQ/viewform" 
         
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import '@justinribeiro/lite-youtube';
+
+const videos = [
+  { id: 'DCFqroxAcV0', title: 'Hanine - Egypt مصر' },
+  { id: 'dx5iKA87Wpk', title: 'Haifa Wehbe - Ma Badعafsh - Esmeralda' },
+  { id: 'dtoKbrb7CT8', title: 'BOYD - Violet - Athena' },
+  { id: 'GIXDSIq5hLI', title: 'Fakerne - Haifa Wahbe - Janet' }
+];
 
 const Video = () => {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">En Construcción</h1>
-      {/* Aquí iría el contenido de la página de videos */}
+      <h1 className="text-3xl font-bold mb-8 text-center">Producciones Audiovisuales</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:grid-cols-2 xl:grid-cols-3 max-w-7xl mx-auto">
+        {/* Mapeamos el array de videos para renderizarlos */}
+        {videos.map((video) => (
+          <div
+            key={video.id}
+            className="w-full transform transition-all duration-300 hover:opacity-50" // Efecto hover
+          >
+            <h2 className="text-xl font-semibold mb-2">{video.title}</h2>
+            <lite-youtube
+              videoid={video.id}
+              short
+              style={{ width: '100%', height: 'auto' }}
+            ></lite-youtube>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
